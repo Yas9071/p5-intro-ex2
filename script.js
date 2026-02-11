fc66b08f62dea1ed647118e179e50d49839faf4b
Yas9071 <yasmin.arabi907@gmail.com>
2026-02-11 06:53:05 +0000

    ex2 done

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* CODE GOES HERE */
+var list = [];
+function preload() {
+  list = loadJSON("data.json");
+}
+function setup() {
+  createCanvas(400, 650);
+}
+function draw() {
+  background(220);
+  textAlign("center");
+  textSize(24);
+  text("Canada's Exports", 200, 40);
+
+  Object.values(list).map((item, index) => {
+    fill(item.color);
+    rect(50, index * 30 + 70, item.amount / 180 * 300, 10);
+
+    // 180 / 180 = 100%
+    // 37 / 180 = 21%
+    textSize(10);
+    textAlign("left");
+    fill("black");
+    text(item.name, 50, index * 30 + 65);
+
+    textSize(20);
+    textAlign("right");
+    text(item.icon, 45, index * 30 + 75);
+
+  });
+
+}
